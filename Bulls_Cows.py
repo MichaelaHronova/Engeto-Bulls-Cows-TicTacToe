011f5d4d34ffbf45f4a8653618a0a1b2b1fa6ec3
laptop\Michaela <hronova.michaela09@gmail.com>
2023-01-29 15:21:54 +0100

    Definice vlastni funkce

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -15,10 +15,16 @@ Let's play a bulls and cows game.
 Enter a number:
 -----------------------------------------------""")
 
+def check_unique_digits(string: str) -> bool:
+    """kontrola unikatnosti secret_number""" #docstring
+    if len(string) == len(set(string)):
+        return True
+    else: 
+        return False
+
 while True:
     secret_number = str(random.randint(1000,9999))
-    secret_set = set(secret_number)
-    if len(secret_number) == len(secret_set): # kontrola unikatnosti secret_number
+    if check_unique_digits(secret_number): 
         break
 
 while True:
@@ -32,7 +38,7 @@ while True:
     if len(number) != 4:
         print("The number must contain 4 digits. Please, try again.")
         continue
-    if len(set(number)) != len(number):
+    if not check_unique_digits(number):
         print ("The number cannot contain duplicities. Please, try again.")
         continue
 
