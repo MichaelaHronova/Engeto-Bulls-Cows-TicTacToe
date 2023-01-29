0fb853454eab0c9973cb2286e85f99e0e0249557
laptop\Michaela <hronova.michaela09@gmail.com>
2023-01-29 14:22:25 +0100

    Unikatni tajne cislo

--- Bulls_Cows.py
+++ Bulls_Cows.py
@@ -4,3 +4,22 @@ author: Michaela Hronová
 email: hronova.michaela09@gmail.com; hronova@ceps.cz
 discord: Míša H.#5316
 '''
+
+import random
+
+print("""Hi there!
+-----------------------------------------------
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+-----------------------------------------------
+Enter a number:
+-----------------------------------------------""")
+
+while True:
+    secret_number = str(random.randint(1000,9999))
+    secret_set = set(secret_number)
+    if len(secret_number) == len(secret_set): # kontrola unikatnosti secret_number
+        break
+
+number = input(">>> ")
+
